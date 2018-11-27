5eea425e965ad2754beb4e34831944a1723547c7
Daniel Roger Hansen <daniel.brann@hotmail.no>
2018-11-27 16:41:26 +0100

    .

---
+++ Script/script.js
@@ -0,0 +1,10 @@
+function openMenu() {
+    document.getElementById("menu").style.width = "350px";
+    document.getElementById("menu").style.display = "block";
+    //document.getElementById("sleepInClass").style.marginRight = "350px";
+}
+
+function closeMenu() {
+    document.getElementById("menu").style.width = "0";
+    //document.getElementById("sleepInClass").style.marginRight = "0"; 
+}
